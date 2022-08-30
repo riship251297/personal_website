7ac076f9d1f45c1579317c05db4c91b9d40eb0ae
Rishikesh Phatangare <riship251297@gmail.com>
2022-08-30 18:50:14 -0400

    changes

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -3,7 +3,7 @@ import '../Photo/Photo.css'
 function Photo() {
     return(
         <div className="photo">
-        <img src={"images/rishi.jpeg"} className="img-circled"width="360px"height="450px"padding="2px"alt=''/>
+        <img src={"images/rishi.png"} className="img-circled"width="360px"height="450px"padding="2px"alt=''/>
         
         </div>
     );
