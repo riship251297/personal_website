478006586e44baf83bc98c9d9c8a457eed592dd0
Rishikesh Phatangare <riship251297@gmail.com>
2022-09-05 00:56:18 -0400

    changes

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -85,104 +85,107 @@ const fetch_image = ()=>
       <div className="title">
         <h3>Welcome to my Gallery section !!!</h3>
       </div>
+      <div className="main">
 
-      <div className="card_section">
-        <Card style={{ width: '35rem' }}>
-        <Card.Img variant="top" src={"images/portfolio.jpg"} />
-        <Card.Body>
-          <Card.Title></Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      </div>
+        <div className="card_section">
+          <Card style={{ width: '35rem' }}>
+          <Card.Img variant="top" src={"images/portfolio.jpg"} />
+          <Card.Body>
+            <Card.Title>Savannah, Georgia</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </div>
 
-      <div className="card_section">
-        <Card style={{ width: '35rem' }}>
-        <Card.Img variant="top" src={"images/car.png"} />
-        <Card.Body>
-          <Card.Title></Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      </div>
+        <div className="card_section">
+          <Card style={{ width: '35rem' }}>
+          <Card.Img variant="top" src={"images/FullSizeRender.png"} />
+          <Card.Body>
+            <Card.Title></Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </div>
 
-      <div className="card_section">
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={"images/car.png"} />
-        <Card.Body>
-          <Card.Title></Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      </div>
+        <div className="card_section">
+          <Card style={{ width: '35rem' }}>
+          <Card.Img variant="top" src={"images/IMG_2001.png"} />
+          <Card.Body>
+            <Card.Title></Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </div>
 
-      <div className="card_section">
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={"images/car.png"} />
-        <Card.Body>
-          <Card.Title></Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      </div>
+        <div className="card_section">
+          <Card style={{ width: '35rem' }}>
+          <Card.Img variant="top" src={"images/IMG_2121.png"} />
+          <Card.Body>
+            <Card.Title></Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </div>
 
-      <div className="card_section">
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={"images/car.png"} />
-        <Card.Body>
-          <Card.Title></Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      </div>
+        <div className="card_section">
+          <Card style={{ width: '35rem' }}>
+          <Card.Img variant="top" src={"images/IMG_2198.png"} />
+          <Card.Body>
+            <Card.Title></Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </div>
 
-      <div className="card_section">
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={"images/car.png"} />
-        <Card.Body>
-          <Card.Title></Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      </div>
+        <div className="card_section">
+          <Card style={{ width: '35rem' }}>
+          <Card.Img variant="top" src={"images/IMG_2500.jpg"} />
+          <Card.Body>
+            <Card.Title></Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </div>
 
-      <div className="card_section">
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={"images/car.png"} />
-        <Card.Body>
-          <Card.Title></Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+        <div className="card_section">
+          <Card style={{ width: '35rem' }}>
+          <Card.Img variant="top" src={"images/IMG_2619.png"} />
+          <Card.Body>
+            <Card.Title></Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
       </div>
+    </div>
      
     </>
   )
