769a07aef4846f3679f21d5891c1503f88445ba0
Rishikesh Phatangare <riship251297@gmail.com>
2022-09-11 01:28:26 -0400

    changes

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,6 +4,8 @@ import Navbar from '../Navbar/Navbar'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import '../Gallery/Gallery.css'
+import Swal from 'sweetalert2'
+
 
 
 
@@ -113,19 +115,28 @@ const [counter_like, setCounterlike] = useState([10, 103, 3, 49, 35, 18, 4, 87, 
 const [counter_share, setCountershare] = useState([1, 29, 37, 44, 50, 82, 40, 7, 6, 59]);
 
 
-function onIncrement(index,type) 
+function onIncrement(index,type,event) 
 {
   if (type == 'download')
-  {
+  {   
+    event.target.style.backgroundColor = "green"
+    // event.currentTarget.disabled = true;
     const countersCopy = [...counter_download];
     countersCopy[index] += 1;
     setCounterdownload(countersCopy);
   }
   if (type == 'like')
   {
+    event.target.style.backgroundColor = "green"
+    event.currentTarget.disabled = true;
     const countersCopy = [...counter_like];
     countersCopy[index] += 1;
     setCounterlike(countersCopy);
+     
+    Swal.fire(
+      'Thanks for liking the picture !!!',
+      'I really appreciate it !!',
+      'success')
   }
   if (type == 'share')
   {
@@ -158,11 +169,11 @@ function onIncrement(index,type)
                 <Card.Title>Old Fort Jackson, Savannah, Georgia</Card.Title>
                 <Card.Text>A day well spent to understand the history of the fort and how it participated in the World War II.</Card.Text>
                 <div className="buttons_section">
-                  <Button className='download'href="images/portfolio.jpg"download="Savannah_trip"onClick={() => onIncrement(0,'download')}>Download | {counter_download[0]}</Button>
-                  <Button className='like'onClick={() => onIncrement(0,'like')}>Like | {counter_like[0]}</Button>
+                  <Button className='download'href="images/portfolio.jpg"download="Savannah_trip"onClick={(event) => onIncrement(0,'download',event)}>Download | {counter_download[0]}</Button>
+                  <Button className='like'onClick={(event) => onIncrement(0,'like',event)}>Like | {counter_like[0]}</Button>
                   <Button className='share'onClick={() => onIncrement(0,'share')}>Share | {counter_share[0]}</Button>
                   <a href="https://en.wikipedia.org/wiki/Savannah,_Georgia">
-                    <Button className='read_mo' >Read more </Button>
+                    <Button className='read' >Read more </Button>
                   </a>
                 </div>
               </Card.Body>
@@ -176,9 +187,9 @@ function onIncrement(index,type)
               <Card.Title>Brooklyn Bridge, New York</Card.Title>
               <Card.Text>Probably one of the best evenings spent on this bridge. Really lucky to have my foot set on this bridge. I wish everyone could have a walk on this bridge with their loved ones...            </Card.Text>
               <div className="buttons_section">
-              <Button className='download'href="images/FullSizeRender.png"download="Brooklyn_Bridge"onClick={() => onIncrement(1,'download')}>Download | {counter_download[1]}</Button>
-                  <Button className='like'onClick={() => onIncrement(1,'like')}>Like | {counter_like[1]}</Button>
-                  <Button className='share'onClick={() => onIncrement(1,'share')}>Share | {counter_share[1]}</Button>
+              <Button className='download'href="images/FullSizeRender.png"download="Brooklyn_Bridge"onClick={(event) => onIncrement(1,'download',event)}>Download | {counter_download[1]}</Button>
+                  <Button className='like'onClick={(event) => onIncrement(1,'like',event)}>Like | {counter_like[1]}</Button>
+                  <Button className='share'onClick={(event) => onIncrement(1,'share',event)}>Share | {counter_share[1]}</Button>
                   <a href="https://en.wikipedia.org/wiki/Brooklyn_Bridge">
                     <Button className='read' >Read more </Button>
                   </a>
@@ -198,9 +209,9 @@ function onIncrement(index,type)
               Kananaskis Country is a park system situated to the west of Calgary, Alberta, Canada in the foothills and front ranges of the Canadian Rockies. The park is home to a myriad of activities and adventures indicative of a mountain park as well as ski hills, camping, numerous day use areas and trails.
               </Card.Text>
               <div className="buttons_section">
-              <Button className='download'href="images/IMG_2001.png"download="Kananaskis"onClick={() => onIncrement(2,'download')}>Download | {counter_download[2]}</Button>
-                  <Button className='like'onClick={() => onIncrement(2,'like')}>Like | {counter_like[2]}</Button>
-                  <Button className='share'onClick={() => onIncrement(2,'share')}>Share | {counter_share[2]}</Button>
+              <Button className='download'href="images/IMG_2001.png"download="Kananaskis"onClick={(event) => onIncrement(2,'download',event)}>Download | {counter_download[2]}</Button>
+                  <Button className='like'onClick={(event) => onIncrement(2,'like',event)}>Like | {counter_like[2]}</Button>
+                  <Button className='share'onClick={(event) => onIncrement(2,'share',event)}>Share | {counter_share[2]}</Button>
                   <a href="https://en.wikipedia.org/wiki/Kananaskis_Country">
                     <Button className='read' >Read more </Button>
                   </a>
@@ -218,7 +229,7 @@ function onIncrement(index,type)
               The World of Coca-Cola is a museum, located in Atlanta, Georgia, showcasing the history of the Coca-Cola Company. The 20-acre complex opened to the public on May 24, 2007, relocating from and replacing the original exhibit, which was founded in 1990 in Underground Atlanta.
               </Card.Text>
               <div className="buttons_section">
-              <Button className='download'href="images/portfolio.jpg"download="Savannah_trip"onClick={() => onIncrement(3,'download')}>Download | {counter_download[3]}</Button>
+              <Button className='download'href="images/portfolio.jpg"download="Savannah_trip"onClick={(event) => onIncrement(3,'download',event)}>Download | {counter_download[3]}</Button>
                   <Button className='like'onClick={() => onIncrement(3,'like')}>Like | {counter_like[3]}</Button>
                   <Button className='share'onClick={() => onIncrement(3,'share')}>Share | {counter_share[3]}</Button>
                   <a href="https://en.wikipedia.org/wiki/World_of_Coca-Cola">
@@ -241,7 +252,7 @@ function onIncrement(index,type)
                 bulk of the card's content.
               </Card.Text>
               <div className="buttons_section">
-              <Button className='download'href="images/portfolio.jpg"download="Savannah_trip"onClick={() => onIncrement(4,'download')}>Download | {counter_download[4]}</Button>
+              <Button className='download'href="images/portfolio.jpg"download="Savannah_trip"onClick={(event) => onIncrement(4,'download',event)}>Download | {counter_download[4]}</Button>
                   <Button className='like'onClick={() => onIncrement(4,'like')}>Like | {counter_like[4]}</Button>
                   <Button className='share'onClick={() => onIncrement(4,'share')}>Share | {counter_share[4]}</Button>
                   <a href="https://google.com">
