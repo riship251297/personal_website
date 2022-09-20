3bd6112a0ea439c8d17a7771653a5c58fd9e62a5
Rishikesh Phatangare <riship251297@gmail.com>
2022-09-20 04:39:57 -0400

    changes

--- src/components/Research/Research.jsx
+++ src/components/Research/Research.jsx
@@ -79,15 +79,15 @@ function Research()
                                             <h4 className="opol"> Face and brain tissue probability</h4>
                                         </div>
                                         <div className="io">
-                                            <img src={"images/rishi_0.2.png"} className="img-circled"width="900px"height="400px"alt=''/>
+                                            <img src={"images/rishi_0.2.png"} className="img-circled"width="850px"height="400px"alt=''/>
                                             <h4>0.8 and 0.3</h4>
                                         </div>
                                         <div className="uo">
-                                            <img src={"images/rishi_0.3.png"} className="img-circled"width="900px"height="400px"alt=''/>
+                                            <img src={"images/rishi_0.3.png"} className="img-circled"width="850px"height="400px"alt=''/>
                                             <h4>0.35 and 0.2</h4>
                                         </div>
                                         <div className="yo">
-                                            <img src={"images/rishi_0.4.png"} className="img-circled"width="900px"height="400px"alt=''/>   
+                                            <img src={"images/rishi_0.4.png"} className="img-circled"width="850px"height="400px"alt=''/>   
                                             <h4>0.17 and 0.12</h4>
                                         </div>
                                       
