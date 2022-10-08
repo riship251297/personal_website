fd205dc68e45fbbacce8994db42aa076794af496
Rishikesh Phatangare <riship251297@gmail.com>
2022-10-08 15:03:29 -0400

    changes

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -7,11 +7,52 @@ function Main() {
     return(
         <>
         <Topbar/>
-        <div className="resume-wrapper">
-            <a href="https://personalwebsite9.s3.amazonaws.com/resume_rishikesh_251297.pdf"download='Rishikesh_resume'>
-                <button type="button" className="btn btn-dark">Download Resume</button>
+        <br></br>
+
+
+        <div className="social_wrapper">
+        <div>
+          <h4>
+            {" "}
+          
+            <a
+              href="https://personalwebsite9.s3.amazonaws.com/resume_rishikesh_251297.pdf"
+              download="Rishikesh_resume"
+            >
+              <button type="button" className="btn btn-success">
+                Resume
+              </button>
             </a>
+          </h4>
         </div>
+        <br></br>
+
+        <div>
+          <h4>
+
+            <a
+              href="https://github.com/riship251297"
+            >
+              <button type="button" className="btn btn-dark">
+                Github
+              </button>
+            </a>
+          </h4>
+        </div>
+
+        <h4>
+          
+          <a
+            href="https://www.linkedin.com/in/rishi251297/"
+            download="Rishikesh_resume"
+          >
+            <button type="button" className="btn btn-primary">
+              LinkedIn
+            </button>
+          </a>
+        </h4>
+        </div>
+
 
 
         <div className="main_wrapper">
