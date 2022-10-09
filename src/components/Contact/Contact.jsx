f7beae6d39232d0de3033ac8800fc98a37d69e93
Rishikesh Phatangare <riship251297@gmail.com>
2022-10-09 03:10:31 -0400

    changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,110 +1,103 @@
-import React from 'react'
-import axios from 'axios'
-import Navbar from '../Navbar/Navbar'
-import '../Contact/Contact.css'
+import React from "react";
+import axios from "axios";
+import Navbar from "../Navbar/Navbar";
+import "../Contact/Contact.css";
 // import {EmailShareButton,WhatsappShareButton,FacebookShareButton,LinkedinShareButton, LinkedinIcon} from 'react-share'
 // import {FacebookIcon} from 'react-share'
-import { Link } from 'react-router-dom'
-import  {API} from 'aws-amplify'
-import {Container,Button,Form} from 'react-bootstrap';
-import Topbar from '../Topbar/Topbar'
-import Swal from 'sweetalert2'
+import { Link } from "react-router-dom";
+import { API } from "aws-amplify";
+import { Container, Button, Form } from "react-bootstrap";
+import Topbar from "../Topbar/Topbar";
+import Swal from "sweetalert2";
 
+const formState = { username: "", email: "", message: "" };
 
-const formState = { username: '', email: '', message: '' };
-  
-  function updateFormState(key, value) {
-    formState[key] = value;
-  }
+function updateFormState(key, value) {
+  formState[key] = value;
+}
 
-function Contact() 
-{
+function call() {
+  setTimeout(function () {
+    window.location.href = "/";
+  }, 4000);
+}
 
+function Contact() {
   function addContact() {
     const data = {
       body: {
         username: formState.username,
         email: formState.email,
-        message: formState.message
-      }
+        message: formState.message,
+      },
     };
-  
-    API.post('rphatan', '/sendemail', data);
-    // Swal.fire({
-    //   'Thanks for providing your details !!!',
-    //   'I really appreciate it !!',
-    //   'success'})
 
-      Swal.fire({
-        position: 'center',
-        icon: 'success',
-        title: 'Your work has been saved',
-        showConfirmButton: false,
-        timer: 1500
-      })
+    API.post("rphatan", "/sendemail", data);
 
-    // setTimeout(function () {
-    //   window.location.href = '/'
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "THANKS FOR PROVIDING DETAILS !!!",
+      showConfirmButton: true,
+      timer: 5000,
+    });
 
-       
-    // }, 5000);
-  
-    
-    window.location.href = '/'
-    // console.log({ apiData });
-    // alert('Mail sent');
-
-  //   Swal.fire(
-  //     'Thanks for !!!',
-  //     'I really appreciate it !!',
-  //     'success')
-  // }
+    call();
   }
 
   return (
     <>
-    {/* <Navbar/> */}
-    <Topbar/>
-            <Container>
+      {/* <Navbar/> */}
+      <Topbar />
+      <Container>
+        <div className="heading">
+          <div className="text">
+            <h3 className="popl">Let's Talk .... ?</h3>
+            <h3 className="lop">
+              I would love to hear from you. Get in touch with me by providing
+              <br></br> your details and I will reach out to you at the
+              earliest.
+            </h3>
+          </div>
+          <div className="group">
+            <h3 className="middle">DROP ME A MESSAGE !</h3>
 
-    <div className="heading">
-      <div className="text">
-        <h3 className="popl">Let's Talk ....  ?</h3>
-        <h3 className="lop">I would love to hear from you. Get in touch with me by providing<br></br> your details and I will reach out to you at the earliest.</h3>
-      </div>
-      <div className="group">
-        <h3 className='middle'>DROP ME A MESSAGE !</h3>
+            <Form>
+              <Form.Group>
+                <Form.Label>Name:</Form.Label>
+                <Form.Control
+                  placeholder="Enter your Name"
+                  onChange={(e) => updateFormState("username", e.target.value)}
+                />
+              </Form.Group>
+              <br></br>
 
-          <Form>
-
-          <Form.Group>
-            <Form.Label>Name:</Form.Label>
-            <Form.Control
-                placeholder="Enter your Name"
-                onChange={e=>  updateFormState('username', e.target.value)} /> 
-          </Form.Group>
-          <br></br>
-
-          <Form.Group>
-            <Form.Label>Email:</Form.Label>
-            <Form.Control
-                placeholder="Enter your Email"
-                onChange={e=>  updateFormState('email', e.target.value)} /> 
-          </Form.Group>
-          <br></br>
-          <Form.Group>
-            <Form.Label>Message:</Form.Label>
-            <Form.Control as="textarea" rows={7}
-                placeholder="Enter your message"
-                onChange={e=>  updateFormState('message', e.target.value)} /> 
-          </Form.Group>
-          <br></br>
-          <br></br>
-          <Button style={{marginLeft:95}} onClick={addContact}>Send me a message</Button>
-          </Form>
+              <Form.Group>
+                <Form.Label>Email:</Form.Label>
+                <Form.Control
+                  placeholder="Enter your Email"
+                  onChange={(e) => updateFormState("email", e.target.value)}
+                />
+              </Form.Group>
+              <br></br>
+              <Form.Group>
+                <Form.Label>Message:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={7}
+                  placeholder="Enter your message"
+                  onChange={(e) => updateFormState("message", e.target.value)}
+                />
+              </Form.Group>
+              <br></br>
+              <br></br>
+              <Button style={{ marginLeft: 95 }} onClick={addContact}>
+                Send me a message
+              </Button>
+            </Form>
+          </div>
         </div>
-      </div>
-      {/* <FacebookShareButton
+        {/* <FacebookShareButton
           url = "https://www.facebook.com">
           <FacebookIcon logoFillColor="white" round={true}></FacebookIcon>
         </FacebookShareButton>
@@ -113,20 +106,11 @@ function Contact()
           <LinkedinIcon round={true}></LinkedinIcon>
 
         </LinkedinShareButton> */}
-    
-    <div className='footer'>
-      
-    </div> 
-    </Container>
+
+        <div className="footer"></div>
+      </Container>
     </>
-  )
+  );
 }
 
-export default Contact
-
-
-  
-
-
-
-
+export default Contact;
